776fea357fe3b3018931f4af1b1128c69befa2b8
chan <98894998+longrunpc@users.noreply.github.com>
2025-12-08 15:19:30 +0900

    feat: 멀티모듈 아키텍처 기본 구조 설정 및 템플릿 수정 (#3)
    * feat: 멀티모듈 아키텍처 기본 구조 설정 (#2)
    
    - domain, application, infrastructure, presentation, common 모듈 기본 구조 세팅
    
    * chore: GitHub 템플릿 파일 위치 변경
    
    이슈 및 풀 리퀘스트 템플릿 파일의 위치를 변경하였습니다.
    기존의 qootalk-server/.github 경로에서 .github 경로로 이동하였습니다.

--- qootalk-server/module-presentation/src/test/java/com/lrchan/qootalk/presentation/QootalkApplicationTests.java
+++ qootalk-server/module-presentation/src/test/java/com/lrchan/qootalk/presentation/QootalkApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lrchan.qootalk;
+package com.lrchan.qootalk.presentation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class QootalkApplicationTests {
 	}
 
 }
+
