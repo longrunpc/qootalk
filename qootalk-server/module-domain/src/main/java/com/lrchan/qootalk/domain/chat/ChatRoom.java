fc1c5e19c846253c68a0e5bf2db504cfbd27e088
longrunpc <pcjmo4050@gmail.com>
2025-12-09 14:12:17 +0900

    feat: 채팅방 모델(ChatRoom) 추가
    채팅방을 나타내는 ChatRoom 클래스를 새로 추가하였습니다.
    이 클래스는 방 이름, 방 유형, 생성자 정보를 포함하며, 방 이름 변경 및 생성자 변경 기능을 제공합니다.

---
+++ qootalk-server/module-domain/src/main/java/com/lrchan/qootalk/domain/chat/ChatRoom.java
@@ -0,0 +1,49 @@
+package com.lrchan.qootalk.domain.chat;
+
+import java.time.LocalDateTime;
+
+import com.lrchan.qootalk.domain.common.BaseModel;
+
+public class ChatRoom extends BaseModel {
+
+    private RoomName roomName;
+    private RoomType roomType;
+    private Long createBy;
+
+    private ChatRoom(Long id, RoomName roomName, RoomType roomType, Long createBy, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+        super(id, createdAt, updatedAt, deletedAt);
+        this.roomName = roomName;
+        this.roomType = roomType;
+        this.createBy = createBy;
+    }
+
+    public static ChatRoom create(String roomName, RoomType roomType, Long createBy) {
+        return new ChatRoom(null, new RoomName(roomName), roomType, createBy, LocalDateTime.now(), LocalDateTime.now(), null);
+    }
+
+    public RoomName roomName() {
+        return roomName;
+    }
+
+    public RoomType roomType() {
+        return roomType;
+    }
+
+    public Long createBy() {
+        return createBy;
+    }
+
+    public void changeRoomName(String roomName) {
+        this.roomName = new RoomName(roomName);
+        update();
+    }
+
+    public void changeCreateBy(Long createBy) {
+        this.createBy = createBy;
+        update();
+    }
+
+    public void softDelete() {
+        super.softDelete();
+    }
+}
