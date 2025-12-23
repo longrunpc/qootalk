78a65e1679f571c70c174fe8f379576574119998
longrunpc <pcjmo4050@gmail.com>
2025-12-23 23:02:38 +0900

    feat: 콘텐츠 타입 검증 로직 개선 및 에러 코드 추가
    ChatErrorCode에 새로운 에러 코드를 추가하고, ContentType 클래스의 예외 처리에서 IllegalArgumentException 대신 DomainException을 사용하도록 수정했습니다. 이를 통해 콘텐츠 타입 검증 시 보다 명확한 에러 처리가 가능해졌습니다.

--- qootalk-server/module-domain/src/main/java/com/lrchan/qootalk/domain/chat/error/ChatErrorCode.java
+++ qootalk-server/module-domain/src/main/java/com/lrchan/qootalk/domain/chat/error/ChatErrorCode.java
@@ -11,7 +11,8 @@ public enum ChatErrorCode implements ErrorCode {
     CHAT_ROOM_PARTICIPANT_INVALID_LAST_READ_MESSAGE_ID("CHAT_005", "마지막 읽은 메시지 ID가 올바르지 않습니다."),
 
     CHAT_FILE_METADATA_INVALID_STORAGE_TYPE("CHAT_006", "파일 메타데이터 스토리지 타입이 올바르지 않습니다."),
-    CHAT_FILE_METADATA_INVALID_FILE_NAME("CHAT_007", "파일 이름이 올바르지 않습니다.");
+    CHAT_FILE_METADATA_INVALID_FILE_NAME("CHAT_007", "파일 이름이 올바르지 않습니다."),
+    CHAT_FILE_METADATA_INVALID_CONTENT_TYPE("CHAT_008", "파일 메타데이터 콘텐츠 타입이 올바르지 않습니다.");
 
     private final String code;
     private final String message;
