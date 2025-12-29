4caf07f6fd5f918b7a82632f94952bd894014853
longrunpc <pcjmo4050@gmail.com>
2025-12-29 16:27:22 +0900

    feat: User Entity 클래스 추가
    사용자 정보를 관리하기 위한 UserEntity 클래스를 추가하였습니다.
    이 클래스는 이메일, 비밀번호, 이름, 프로필 이미지 URL, 상태 메시지 및 사용자 역할을 포함합니다.
    Hibernate를 사용하여 데이터베이스와의 매핑을 설정하였습니다.

---
+++ qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/persistence/user/UserEntity.java
@@ -0,0 +1,69 @@
+package com.lrchan.qootalk.infrastructure.persistence.user;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.lrchan.qootalk.domain.user.UserRole;
+import com.lrchan.qootalk.infrastructure.persistence.common.BaseEntity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+@SQLDelete(sql = "UPDATE users SET deleted_at = now() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+public class UserEntity extends BaseEntity {
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "status_message")
+    private String statusMessage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private UserRole role;
+
+    protected UserEntity() {
+    }
+
+    public UserEntity(String email, String password, String name, UserRole role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public String password() {
+        return password;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String profileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public String statusMessage() {
+        return statusMessage;
+    }
+
+    public UserRole role() {
+        return role;
+    }
+}
