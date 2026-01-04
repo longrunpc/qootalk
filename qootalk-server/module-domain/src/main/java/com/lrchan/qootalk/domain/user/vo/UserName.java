821d41c55641a476eca0879f2477e7210b80c2e7
longrunpc <pcjmo4050@gmail.com>
2026-01-04 14:50:49 +0900

    refactor: UserName VO 불변객체로 변환

--- qootalk-server/module-domain/src/main/java/com/lrchan/qootalk/domain/user/vo/UserName.java
+++ qootalk-server/module-domain/src/main/java/com/lrchan/qootalk/domain/user/vo/UserName.java
@@ -3,9 +3,13 @@ package com.lrchan.qootalk.domain.user.vo;
 import com.lrchan.qootalk.common.exception.DomainException;
 import com.lrchan.qootalk.domain.user.error.UserErrorCode;
 
-public class UserName {
+public final class UserName {
     
-    private String value;
+    private final String value;
+
+    protected UserName() {
+        this.value = null;
+    }
 
     public UserName(String value) {
         validate(value);
@@ -23,5 +27,18 @@ public class UserName {
         if (value.length() < 2 || value.length() > 20) {
             throw new DomainException(UserErrorCode.USER_INVALID_NAME);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserName)) return false;
+        UserName userName = (UserName) o;
+        return java.util.Objects.equals(value, userName.value);
+    }
+    
+    @Override
+    public int hashCode() {
+        return value.hashCode();
     }
 }
