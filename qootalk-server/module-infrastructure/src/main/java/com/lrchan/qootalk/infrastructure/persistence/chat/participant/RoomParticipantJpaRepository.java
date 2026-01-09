1ba42db2e56790e87cccb85fb850187f98c61587
longrunpc <pcjmo4050@gmail.com>
2026-01-09 17:24:58 +0900

    test: RoomParicipant Entity 매퍼 테스트 추가

--- qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/persistence/chat/participant/RoomParticipantJpaRepository.java
+++ qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/persistence/chat/participant/RoomParticipantJpaRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomParticipnatJpaRepository extends JpaRepository<RoomParticipantEntity, Long> {
+public interface RoomParticipantJpaRepository extends JpaRepository<RoomParticipantEntity, Long> {
     Optional<RoomParticipantEntity> findByUserIdAndRoomId(Long userId, Long roomId);
     boolean existsByUserIdAndRoomId(Long userId, Long roomId);
 }
