6322df6129f49aa56f326f329e51264fa706a548
longrunpc <pcjmo4050@gmail.com>
2026-01-09 18:50:22 +0900

    chore: MessageJpaRepository에서 findById 메서드 삭제
    findById 메서드가 불필요하게 남아있어 삭제하였습니다.
    이로 인해 코드의 간결성을 높였습니다.

--- qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/persistence/chat/message/MessageJpaRepository.java
+++ qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/persistence/chat/message/MessageJpaRepository.java
@@ -5,5 +5,4 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageJpaRepository extends JpaRepository<MessageEntity, Long> {
-    Optional<MessageEntity> findById(Long id);
 }
