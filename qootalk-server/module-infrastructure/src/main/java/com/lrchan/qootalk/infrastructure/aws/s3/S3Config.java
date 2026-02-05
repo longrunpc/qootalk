9944250cc4f43039ecd96feca4feca57ab1d842e
longrunpc <pcjmo4050@gmail.com>
2026-02-05 11:21:05 +0900

    feat: AWS S3 설정LocalStack 전용으로 설정

---
+++ qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/aws/s3/S3Config.java
@@ -0,0 +1,25 @@
+package com.lrchan.qootalk.infrastructure.aws.s3;
+
+import java.net.URI;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+            .endpointOverride(URI.create("http://localhost:4566"))
+            .region(Region.AP_NORTHEAST_2)
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("accessKey", "secretKey")))
+            .forcePathStyle(true)
+            .build();
+    }
+}
