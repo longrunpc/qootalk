a1369eaea312c64fa16065bf262e5555315216ed
longrunpc <pcjmo4050@gmail.com>
2026-02-05 14:25:05 +0900

    fix: S3ErrorCode S3_FILE_DELETE_FAILED 오타 수정

--- qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/persistence/common/error/S3ErrorCode.java
+++ qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/persistence/common/error/S3ErrorCode.java
@@ -5,7 +5,7 @@ import com.lrchan.qootalk.common.error.ErrorCode;
 public enum S3ErrorCode implements ErrorCode {
     
     S3_FILE_UPLOAD_FAILED("S3_001", "S3 파일 업로드 실패하였습니다."),
-    S3_FILE_DELETE_FAILED("S3_003", "S3 파일 삭제 실패하였습니다.");
+    S3_FILE_DELETE_FAILED("S3_002", "S3 파일 삭제 실패하였습니다.");
 
     private final String code;
     private final String message;
