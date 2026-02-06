eb81845bfbf9c10a49f01804ff65e1171bf17114
longrunpc <pcjmo4050@gmail.com>
2026-02-06 15:43:07 +0900

    feat: 프로필 이미지 업로드 Command 및 Usecase 정의
    사용자가 프로필 이미지를 업로드할 수 있도록
    UploadProfileImageCommand 클래스와
    UploadprofileImageUsecase 인터페이스를 추가했습니다.

---
+++ qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/dto/command/UploadProfileImageCommand.java
@@ -0,0 +1,11 @@
+package com.lrchan.qootalk.application.user.dto.command;
+
+import java.io.InputStream;
+
+public record UploadProfileImageCommand(
+    Long userId,
+    InputStream inputStream,
+    String originalFileName,
+    long fileSize
+) {
+}
--- qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/port/in/UploadprofileImageUsecase.java
+++ qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/port/in/UploadprofileImageUsecase.java
@@ -1,5 +1,6 @@
 package com.lrchan.qootalk.application.user.port.in;
 
+import com.lrchan.qootalk.application.user.dto.command.UploadProfileImageCommand;
 import com.lrchan.qootalk.application.user.dto.result.UserQueryResult;
 
 public interface UploadprofileImageUsecase {
