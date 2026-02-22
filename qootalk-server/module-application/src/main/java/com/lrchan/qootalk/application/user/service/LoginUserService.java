3f4709e7eda17327ccb8147ffb8c42cedac7c8fb
longrunpc <pcjmo4050@gmail.com>
2026-02-22 23:46:34 +0900

    feat(Application): login 로직 중 refresh token redis 저장 로직 추가

--- qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/service/LoginUserService.java
+++ qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/service/LoginUserService.java
@@ -11,6 +11,7 @@ import com.lrchan.qootalk.application.user.dto.result.UserQueryResult;
 import com.lrchan.qootalk.application.user.error.UserApplicationErrorCode;
 import com.lrchan.qootalk.application.user.port.in.LoginUserUseCase;
 import com.lrchan.qootalk.application.user.port.out.LoadUserPort;
+import com.lrchan.qootalk.application.user.port.out.RefreshTokenPort;
 import com.lrchan.qootalk.application.user.port.out.TokenProvider;
 import com.lrchan.qootalk.common.exception.ApplicationException;
 import com.lrchan.qootalk.domain.user.User;
@@ -25,6 +26,7 @@ public class LoginUserService implements LoginUserUseCase {
     private final LoadUserPort loadUserPort;
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
+    private final RefreshTokenPort refreshTokenPort;
 
     @Override
     public LoginResult login(LoginUserCommand command) {
@@ -37,6 +39,8 @@ public class LoginUserService implements LoginUserUseCase {
 
         String accessToken = tokenProvider.createAccessToken(String.valueOf(user.id()), user.role().name());
         String refreshToken = tokenProvider.createRefreshToken(String.valueOf(user.id()));
+
+        refreshTokenPort.save(String.valueOf(user.id()), refreshToken);
 
         return LoginResult.of(UserQueryResult.of(user), new TokenResponse(accessToken, refreshToken));
     }
