2e92a634f7cfe35c08add89a9b5d7ee2f66db284
longrunpc <pcjmo4050@gmail.com>
2026-02-23 00:23:43 +0900

    feat(Infrastructure): refresh token 관련 redis 어댑터 구현

---
+++ qootalk-server/module-infrastructure/src/main/java/com/lrchan/qootalk/infrastructure/persistence/redis/RefreshTokenRedisAdapter.java
@@ -0,0 +1,48 @@
+package com.lrchan.qootalk.infrastructure.persistence.redis;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import com.lrchan.qootalk.application.user.port.out.RefreshTokenPort;
+import com.lrchan.qootalk.domain.user.vo.Token;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class RefreshTokenRedisAdapter implements RefreshTokenPort {
+    
+    private final StringRedisTemplate redisTemplate;
+
+    private static final String REFRESH_TOKEN_PREFIX = "refresh_token:";
+
+    @Override
+    public void save(String userPk, Token refreshToken) {
+        String key = REFRESH_TOKEN_PREFIX + userPk;
+
+        redisTemplate.opsForValue().set(
+            Objects.requireNonNull(key), 
+            Objects.requireNonNull(refreshToken.token()),
+            refreshToken.expiresIn(),
+            TimeUnit.MILLISECONDS
+        );
+    }
+
+    @Override
+    public Optional<Token> findByUserPk(String userPk) {
+        String key = REFRESH_TOKEN_PREFIX + userPk;
+        String token = redisTemplate.opsForValue().get(key);
+        long expiresIn = redisTemplate.getExpire(key, TimeUnit.MILLISECONDS);
+        return Optional.ofNullable(new Token(token, Math.max(expiresIn, 0)));
+    }
+
+    @Override
+    public void deleteByUsersPk(String userPk) {
+        String key = REFRESH_TOKEN_PREFIX + userPk;
+        redisTemplate.delete(key);
+    }
+}
