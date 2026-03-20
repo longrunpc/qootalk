ae7fb52c41338e5485796c719bd191992d4301e3
longrunpc <pcjmo4050@gmail.com>
2026-03-20 18:14:46 +0900

    refactor: user 관련 service 로직에 유저 상태 확인 추가

--- qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/service/LoginUserService.java
+++ qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/service/LoginUserService.java
@@ -31,13 +31,19 @@ public class LoginUserService implements LoginUserUseCase {
 
     @Override
     public LoginResult login(LoginUserCommand command) {
+        // 유저 조회
         User user = loadUserPort.findByEmail(command.email().value())
-            .orElse(null);
+            .orElseThrow(() -> new ApplicationException(UserApplicationErrorCode.LOGIN_FAILED));
+        if (user.isDeleted()) {
+            throw new ApplicationException(UserApplicationErrorCode.USER_DELETED);
+        }
 
+        // 비밀번호 검증
         if (user == null || !passwordEncoder.matches(command.password().encryptedPassword(), user.password().encryptedPassword())) {
             throw new ApplicationException(UserApplicationErrorCode.LOGIN_FAILED);
         }
 
+        // 토큰 생성
         Token accessToken = tokenProvider.createAccessToken(String.valueOf(user.id()), user.role().name());
         Token refreshToken = tokenProvider.createRefreshToken(String.valueOf(user.id()));
 
--- qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/service/UploadProfileImageService.java
+++ qootalk-server/module-application/src/main/java/com/lrchan/qootalk/application/user/service/UploadProfileImageService.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Service;
 
 import com.lrchan.qootalk.application.user.dto.command.UploadProfileImageCommand;
 import com.lrchan.qootalk.application.user.dto.result.UserQueryResult;
+import com.lrchan.qootalk.application.user.error.UserApplicationErrorCode;
 import com.lrchan.qootalk.application.user.port.in.UploadProfileImageUsecase;
 import com.lrchan.qootalk.application.user.port.out.DeleteFilePort;
 import com.lrchan.qootalk.application.user.port.out.LoadUserPort;
 import com.lrchan.qootalk.application.user.port.out.SaveUserPort;
 import com.lrchan.qootalk.application.user.port.out.UploadFilePort;
+import com.lrchan.qootalk.common.exception.ApplicationException;
 import com.lrchan.qootalk.common.exception.DomainException;
 import com.lrchan.qootalk.common.storage.vo.StorageResource;
 import com.lrchan.qootalk.domain.user.User;
@@ -31,7 +33,10 @@ public class UploadProfileImageService implements UploadProfileImageUsecase {
     @Override
     public UserQueryResult upload(UploadProfileImageCommand command) {
         User user = loadUserPort.findById(command.userId())
-                        .orElseThrow(() -> new DomainException(UserErrorCode.USER_NOT_FOUND));
+            .orElseThrow(() -> new DomainException(UserErrorCode.USER_NOT_FOUND));
+        if (user.isDeleted()) {
+            throw new ApplicationException(UserApplicationErrorCode.USER_DELETED);
+        }
 
         String path = "profile/" + command.userId();
         StorageResource resource = new StorageResource(
